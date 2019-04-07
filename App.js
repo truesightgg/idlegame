91bb3f3ccf0f46b2ab4124f5384cb5a0696c07e3
Justin Wong <juwong@shaw.ca>
2019-04-06 17:09:05 -0700

    Points class and adder method created

--- App.js
+++ App.js
@@ -1,12 +1,92 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+//
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Current Points</Text>
+//         <button onPress={p.addPoints(1)}>
+//           Click Me!
+//         </button>
+//       </View>
+//     );
+//   }
+// }
+//
+// class Points {
+//   constructor(value) {
+//     this.value = value
+//   }
+//   //Add points method
+//   addPoints(amount) {
+//     this.value += amount
+//   }
+// }
+//
+// const p = new Points(0);
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+import React, { Component } from 'react';
+import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
 
-export default class App extends React.Component {
+class Points {
+  constructor(value) {
+    this.value = value
+  }
+  //Add points method
+  addPoints(amount) {
+    this.value += amount
+  }
+}
+
+const p = new Points(0);
+
+export default class ButtonBasics extends Component {
+  _onPressButton() {
+    Alert.alert('You tapped the button!')
+  }
+  _addPoints() {
+    p.addPoints(1)
+    Alert.alert(p.value.toString())
+  }
+
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app! Do it now!! Please?</Text>
-        <Text>Hello, world!</Text>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._addPoints}
+            title="Press Me"
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+            color="#841584"
+          />
+        </View>
+        <View style={styles.alternativeLayoutButtonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="This looks great!"
+          />
+          <Button
+            onPress={this._onPressButton}
+            title="OK!"
+            color="#841584"
+          />
+        </View>
       </View>
     );
   }
@@ -14,9 +94,18 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#f44242',
-    alignItems: 'center',
-    justifyContent: 'center',
+   flex: 1,
+   justifyContent: 'center',
   },
+  buttonContainer: {
+    margin: 20
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
 });
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => ButtonBasics);
