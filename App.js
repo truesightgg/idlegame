fe18f5fea384e3a0f3f12bbae6da9fba7be8cba8
StevenGin <verdananew@gmail.com>
2019-04-09 19:15:01 -0700

    Cleaned up and simplified code

--- App.js
+++ App.js
@@ -1,70 +1,25 @@
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-//
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text>Current Points</Text>
-//         <button onPress={p.addPoints(1)}>
-//           Click Me!
-//         </button>
-//       </View>
-//     );
-//   }
-// }
-//
-// class Points {
-//   constructor(value) {
-//     this.value = value
-//   }
-//   //Add points method
-//   addPoints(amount) {
-//     this.value += amount
-//   }
-// }
-//
-// const p = new Points(0);
-//
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-import React, { Component } from 'react';
-import { Alert, AppRegistry, Button, StyleSheet, View, Text } from 'react-native';
+import React, { Component } from 'react'
+import { Alert, AppRegistry, Button, StyleSheet, View, Text } from 'react-native'
 
-class Points {
-  constructor(value) {
-    this.value = value
-  }
-  //Add points method
-  addPoints(amount) {
-    this.value += amount
-  }
-}
-
-const p = new Points(0);
 
 
 
 export default class reactApp extends Component {
   constructor() {
-   super()
+    super();
     this.state = {
-      myPoints: "0"
+    coins: 0
     }
+    this.addCoin = this.addCoin.bind(this)
+    setInterval(this.addCoin, 1000)
   }
-  _updateText = () => {
-    p.addPoints(1);
-    this.setState({myText: p.value.toString})
+
+  addCoin() {
+    this.setState(state => ({
+      coins: state.coins + 1
+    }))
   }
-  _onPressButton() {
-    Alert.alert('You tapped the button!')
-  }
+
 
 
 
@@ -75,31 +30,13 @@ export default class reactApp extends Component {
       <View style={styles.container}>
         <View>
         <Text>
-        {p.value}
+        {this.state.coins.toString()}
         </Text>
         </View>
         <View style={styles.buttonContainer}>
           <Button
-            onPress={this._updateText}
+            onPress={this.addCoin}
             title="Press Me"
-          />
-        </View>
-        <View style={styles.buttonContainer}>
-          <Button
-            onPress={this._onPressButton}
-            title="Press Me"
-            color="#841584"
-          />
-        </View>
-        <View style={styles.alternativeLayoutButtonContainer}>
-          <Button
-            onPress={this._onPressButton}
-            title="This looks great!"
-          />
-          <Button
-            onPress={this._onPressButton}
-            title="OK!"
-            color="#841584"
           />
         </View>
       </View>
@@ -114,13 +51,8 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     margin: 20
-  },
-  alternativeLayoutButtonContainer: {
-    margin: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
   }
-});
+})
 
 // skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => ButtonBasics);
+AppRegistry.registerComponent('AwesomeProject', () => ButtonBasics)
